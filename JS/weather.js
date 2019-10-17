42451b28c46c9d8a0fde4121936ec5c0f2246328
joseramirezjimenez <jose.ramirez5019b@gmail.com>
2019-10-17 12:36:41 -0500

    Added template weather card and styling

--- JS/weather.js
+++ JS/weather.js
@@ -1,37 +1,55 @@
-(function() {
-    //getWeatherInfo();
-    geocode();
+(function () {
+    //Submit Button Event Handler
+    $("#submit").click(function () {
+        //Get the value the user has entered into the search bar and store it
+        const searchLocation = $("#searchBar").val();
+        //Call the geocode function and pass in the value
+        geocode(searchLocation);
+        //Clear out the search bar
+        $("#searchBar").val("");
+    })
 })();
+//Function to connect to the Dark Sky API and get weather data
+function getWeatherInfo(latitude, longitude, city, state) {
+    //Base-URL/APIKey/Latitude,Longitude
+    $.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/" + latitude + "," + longitude, { dataType: "jsonp" })
+        .done(function (data) {
+            //See if you can get the following data from the JSON:
 
-//Function to connect to the Dark Sky API and get weather info
-function getWeatherInfo() {
-//https://api.darksky.net/forecast/d04a8913171dae82d56dea4b8e2eba1e/37.8267,-122.4233
-//Base-URL/APIKey/Latitude,Longitude
+            //1. Get the current temp.
 
-$.ajax("https://api.darksky.net/forecast/" + darkSkyKey + "/37.8267,-122.4233",
-{ dataType: "jsonp" })
-.done(function(data) {
-    console.log(data);
-})
-.fail(function(error) {
-    console.log(error);
-})
-.always(function(data) {
-    console.log("Weather call complete!");
-})
+            //2. Get the probability of precipitation
+            //3. Get the high and low temo. for the current day (first element in the data array in the daily object)
+        })
+        .fail(function (error) {
+            console.log(error);
+        })
+        .always(function () {
+            console.log("Weather call complete!");
+        })
 }
+//Function to connect to the MapQuest Geocoding API and get geocoding data
+function geocode(location) {
+    //Base-URL=APIKey + &location= + Adress
+    $.ajax("http://www.mapquestapi.com/geocoding/v1/address?key=" + mapQuestKey + "&location=" + location)
+        .done(function (data) {
+            //Get the lat and lng from the response
+            let locations = data.results[0].locations[0];
 
-//Functionto connect to the MapQuest Geocoding API and geocoding data
-function geocode() {
-    //Base-URL + APIKey + &location + Address
-    $.ajax("http://www.mapquestapi.com/geocoding/v1/address?key=" + mapQuestKey + "&location=Washington,DC")
-    .done(function(data) {
-        console.log(data);
-    })
-    .fail(function(error) {
-        console.log(error);
-    })
-    .always(function() {
-        console.log("Geocoding call finished");
-    })
-}
+            let lat = locations.latLng.lat;
+            let lng = locations.latLng.lng;
+
+            //
+            let city = locations.adminArea5;
+            let state = locations.adminArea3;
+
+            //Pass the lat and lng to our getWeatherInfo function 
+            getWeatherInfo(lat, lng, city, state)
+        })
+        .fail(function (error) {
+            console.log(error);
+        })
+        .always(function () {
+            console.log("Geocoding call finished")
+        })
+    }
